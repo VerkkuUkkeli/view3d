e6ad0ecbe5c94fc149383bd9d44c76bb510407e7
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2018-12-05 13:41:54 +0200

    Updated readme with usage instructions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # view3d
 
 View3D is a command line utility for viewing 3d models on the command line.
+
+usage: ```python3 main.py```
--- main.py
+++ main.py
@@ -8,23 +8,22 @@ from model import Model
 
 def main(stdscr):
 
-    # animation variables
     fs = 0.1   # scale frequency
-    scale = 2
 
     # running flag
     running = True
 
     renderer = Renderer(stdscr)     # init renderer
     model = Model()                 # model to be displayed
+    model.scale = 1.5*np.ones(3)
 
     # program main loop
     while running:
         stdscr.clear()  # clear screen
 
         renderer.render(model)
-        model.rot += np.array([0.03, 0.02, 0.05])
-        model.scale = scale * np.array([ucos(2*np.pi*fs*time.time()), usin(2*np.pi*2*fs*time.time()), 1])
+        model.rot += np.array([0.1, 0.03, 0.07])
+        model.scale = np.ones(3)*ucos(2*fs*np.pi*time.time())
 
         time.sleep(0.1)     # limit framerate to 10fps to avoid glitching
 
